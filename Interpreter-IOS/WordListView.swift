0d20022a250298aa47cac260279acd20cb0f5ae4
Mahamed Adan <adanm6790@gmail.com>
2026-02-28 15:04:01 +0100

    Search function in WordlistView UI done

--- Interpreter-IOS/WordListView.swift
+++ Interpreter-IOS/WordListView.swift
@@ -15,6 +15,8 @@ struct WordListView: View {
     let category: String
     let sourceLanguage: String
     let targetLanguage: String
+    
+    @State private var searchText: String = ""
 
     // Use a dynamically initialized query so we can capture `category`
     @Query private var filteredWords: [MainWord]
@@ -26,7 +28,9 @@ struct WordListView: View {
         self._filteredWords = Query(filter: #Predicate<MainWord> { word in
             word.cat.localizedStandardContains(category)
         })
+        
     }
+
 
     
 
@@ -34,6 +38,23 @@ struct WordListView: View {
 
     var body: some View {
         Text(category)
+        
+        // Sökfält
+        HStack {
+            Image(systemName: "line.3.horizontal.decrease")
+            
+            TextField("Search word...", text: $searchText)
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled(true)
+                .multilineTextAlignment(.center)
+            
+            Image(systemName: "magnifyingglass")
+            
+        }
+        .padding(20)
+        .background(Color.green.opacity(0.2))
+        .clipShape(RoundedRectangle(cornerRadius: 30))
+        .padding(.horizontal)
         List {
             ForEach(filteredWords) { mainWord in
                 WordRowView(
